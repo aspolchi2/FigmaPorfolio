b6d3d734a43927c193f6f0d13a1c0782b6105455
aspolchi <aspolchi@gmail.com>
2022-07-28 15:59:40 -0300

    asd

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -2,21 +2,32 @@ import { GitHub, LinkedIn } from "@mui/icons-material";
 import { Button } from "@mui/material";
 import React from "react";
 import { about } from "../../porfolio";
+import "../sass/about.scss";
 
 const About = () => {
   const { name, role, description, resume, social } = about;
 
-
-  return <div>
-    <h1>Hi! I'm <span>{name}</span></h1>
-    <h2>{role}</h2>
-    <p>{description}</p>
-    <a href={resume} rel="noreferrer" target="_blank">My Resume</a>
-    <a href={social.github} rel="noreferrer" target="_blank"><GitHub/> </a>
-    <a href={social.linkedin} rel="noreferrer" target="_blank"> <LinkedIn /></a>
-
-
-  </div>;
+  return (
+    <div className="about">
+      <h1>
+        Hi! I'm <span>{name}</span>
+      </h1>
+      <h2>{role}</h2>
+      <p>{description}</p>
+      <div className="socials">
+        <a href={resume} rel="noreferrer" target="_blank">
+          My Resume
+        </a>
+        <a href={social.github} rel="noreferrer" target="_blank">
+          <GitHub />{" "}
+        </a>
+        <a href={social.linkedin} rel="noreferrer" target="_blank">
+          {" "}
+          <LinkedIn />
+        </a>
+      </div>
+    </div>
+  );
 };
 
 export default About;
