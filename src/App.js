cd6fd5856c8940642f38302b83d938178bb38378
aspolchi <aspolchi@gmail.com>
2022-08-03 12:47:32 -0300

    cleaning

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import About from "./components/about/About";
 import Header from "./components/header/Header";
-import Projects from "./components/projects/Projects";
 import "./components/sass/global.scss";
 import ProjectsContainer from "./containers/ProjectsContainer";
 import Skills from "./components/skills/Skills";
